72bda71875f4adb38d0282d20e41281e49205f9a
John Granström <granstrom.john@gmail.com>
2018-03-06 13:17:00 +0100

    test: remove mocha-typescript

--- test/parse-stream.ts
+++ test/parse-stream.ts
@@ -1,6 +1,6 @@
-import { suite, test, only } from 'mocha-typescript';
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
+import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import { parseStream } from '../src';
 import { PassThrough } from 'stream';
@@ -8,21 +8,21 @@ import { stringify } from 'zipson';
 
 chai.use(chaiAsPromised);
 
-@suite class ParseStream {
-  @test shouldRejectOnParseError() {
+describe('parse-stream', function() {
+  it('shouldRejectOnParseError', function() {
     const stream = new PassThrough();
     stream.push('broken_stuff');
     stream.push(null);
     return expect(parseStream(stream)).to.eventually.be.rejected;
-  }
+  });
 
-  @test shouldRejectOnStreamError() {
+  it('shouldRejectOnStreamError', function() {
     const stream = new PassThrough();
     setTimeout(() => { stream.emit('error', new Error('nope')); }, 1);
     return expect(parseStream(stream)).to.eventually.be.rejected;
-  }
+  });
 
-  @test shouldParseStream() {
+  it('shouldParseStream', function() {
     const stringified = stringify([1, 2, 3, 4, 5, 6]);
     const stream = new PassThrough();
     setTimeout(() => stream.push(stringified.slice(0, 2)), 1);
@@ -31,5 +31,5 @@ chai.use(chaiAsPromised);
     setTimeout(() => stream.push(stringified.slice(6, 8)), 1);
     setTimeout(() => stream.push(null), 1);
     return expect(parseStream(stream)).to.eventually.deep.equal([1, 2, 3, 4, 5, 6]);
-  }
-}
+  });
+});
--- test/stream-writer.ts
+++ test/stream-writer.ts
@@ -1,24 +1,24 @@
-import { suite, test, only } from 'mocha-typescript';
+import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import { ZipsonStreamWriter } from '../src';
 import { stringify, stringifyTo } from 'zipson';
 
-@suite class ParseStream {
-  @test shouldWriteEachChunkUnbuffered() {
+describe('stream-writer', function() {
+  it('shouldWriteEachChunkUnbuffered', function() {
     const writer = new ZipsonStreamWriter(0);
     const chunks: string[] = [];
     writer.stream.on('data', d => chunks.push(d.toString()));
     stringifyTo([1, 2, 3, 4, 5, 6], writer);
     expect(chunks).to.have.length(8);
     expect(chunks.join('')).to.equal(stringify([1, 2, 3, 4, 5, 6]));
-  }
+  });
 
-  @test shouldWriteEntireDataBuffered() {
+  it('shouldWriteEntireDataBuffered', function() {
     const writer = new ZipsonStreamWriter(1000);
     const chunks: string[] = [];
     writer.stream.on('data', d => chunks.push(d.toString()));
     stringifyTo([1, 2, 3, 4, 5, 6], writer);
     expect(chunks).to.have.length(1);
     expect(chunks.join('')).to.equal(stringify([1, 2, 3, 4, 5, 6]));
-  }
-}
+  });
+});
